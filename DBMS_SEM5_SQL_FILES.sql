9b8093189192936956604621ce42546d8d7e1bcb
excel-sheet-enthusiast <113234254+excel-sheet-enthusiast@users.noreply.github.com>
2022-11-07 04:58:34 +0530

    Add files via upload

---
+++ DBMS_SEM5_SQL_FILES.sql
@@ -0,0 +1,9 @@
+use tnp_portal; 
+create table Opportunity(
+company_name varchar(255) not null, 
+title varchar(255) not null, 
+descrip varchar(2083) not null, 
+stipend int not null, 
+duration varchar(255) not null, 
+joining_date varchar(255) not null, 
+reg_link varchar(2083) not null); 
